49b4010315b7798e958619fdeb47da593e4ebed5
Ridho Tamma <58821061+ridhotamma@users.noreply.github.com>
2021-11-02 20:29:22 +0700

    change logo text

--- src/contexts/StateContextProvider.js
+++ src/contexts/StateContextProvider.js
@@ -1,21 +1,21 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
-const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
 
 export const StateContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
 
   const getResults = async (url) => {
     setLoading(true);
 
     const res = await fetch(`${baseUrl}${url}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": "bd92aa196fmshcf093fc0c4dca39p1b32e5jsn716541e7425e",
       },
     });
 
@@ -26,7 +26,9 @@ export const StateContextProvider = ({ children }) => {
   };
 
   return (
-    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+    <StateContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
       {children}
     </StateContext.Provider>
   );
